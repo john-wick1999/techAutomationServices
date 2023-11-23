ad4b1f92482f0e0aea82e40cad436dc26d1c93e2
aale24 <alejandroperezpestana99@gmail.com>
2023-11-22 23:54:59 -0500

    delete unused file

--- src/data/socialMedia.tsx
+++
@@ -1,7 +0,0 @@
-export const socialMedia = {
-    instagram: "https://www.instagram.com/",
-    facebook: "https://www.facebook.com/",
-    github:
-      "https://github.com/",
-    homepage: "/",
-  };
